ad322ba680058bffa91a455d350dbf071b05a9a5
Rosali <rosali.alfta@gmail.com>
2026-03-05 13:37:51 +0100

    corrected get all users route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,7 +8,7 @@ exports.getAllUsers = async (req, res) => {
   try {
   const users = await User.find().select("_id username");
 
-    
+    res.json(users);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
