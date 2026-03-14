23ef6305d36199a80205a5660349efc9a2cc9ae9
Rosali <rosali.alfta@gmail.com>
2026-03-14 09:19:58 +0100

    removed authmiddleware from validate route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
-const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
-router.get("/validate", authMiddleware, authController.validate);
+router.get("/validate", authController.validate);
 
 module.exports = router;
